4e828889dbd5868534c14e40cf6aed51a8783ef5
unknown <cortexdeveloper@gmail.com>
2021-01-09 21:10:41 +0200

    add project context, boot installer, states ctors

---
+++ src/MemoryCardsPrototype/Assets/Code/Runtime/Infrastructure/StateMachine/States/BootstrapState.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace Code.Runtime.Infrastructure.StateMachine.States
+{
+  public class BootstrapState : IState
+  {
+    public BootstrapState(GameStateMachine stateMachine)
+    {
+        
+    }
+    
+    private const string LobbyScene = "Lobby";
+    
+    public void Enter()
+    {
+      LoadUserSettings();
+      LoadLobbyScene();
+    }
+
+    public void Exit()
+    {
+      
+    }
+
+    private static void LoadLobbyScene()
+    {
+      SceneManager.LoadScene(LobbyScene);
+      
+    }
+
+    private static void LoadUserSettings()
+    {
+      //TODO implement      
+    }
+  }
+}
