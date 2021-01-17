69171a053d7ebfacc39132299c9b20ed0b468730
unknown <cortexdeveloper@gmail.com>
2021-01-17 16:57:04 +0200

    add card game data SO

---
+++ src/MemoryCardsPrototype/Assets/Code/Runtime/CardGame/Data/CardGameConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Code.Runtime.CardGame.Data
+{
+  [CreateAssetMenu(fileName = nameof(CardGameConfiguration), menuName = "CardGame/Data/" + nameof(CardGameConfiguration), order = 0)]
+  public class CardGameConfiguration : ScriptableObject
+  {
+    [field: SerializeField] private int HorizontalCellsCount { get; }
+    [field: SerializeField] private int VerticalCellsCount { get; }
+
+    [field: SerializeField] private float SessionTime { get; }
+  }
+}
