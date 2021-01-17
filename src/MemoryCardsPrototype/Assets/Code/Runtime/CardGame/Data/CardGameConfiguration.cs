413cfc1e016f1eb6d27c1158b319be2ae986575f
unknown <cortexdeveloper@gmail.com>
2021-01-17 17:23:15 +0200

    refactor card game config

--- src/MemoryCardsPrototype/Assets/Code/Runtime/CardGame/Data/CardGameConfiguration.cs
+++ src/MemoryCardsPrototype/Assets/Code/Runtime/CardGame/Data/CardGameConfiguration.cs
@@ -3,11 +3,30 @@
 namespace Code.Runtime.CardGame.Data
 {
   [CreateAssetMenu(fileName = nameof(CardGameConfiguration), menuName = "CardGame/Data/" + nameof(CardGameConfiguration), order = 0)]
-  public class CardGameConfiguration : ScriptableObject
+  public class CardGameConfiguration : ScriptableObject, ICardGameConfiguration
   {
-    [field: SerializeField] private int HorizontalCellsCount { get; }
-    [field: SerializeField] private int VerticalCellsCount { get; }
+    [SerializeField] private int _horizontalCellsCount;
+    public int HorizontalCellsCount => 
+      _horizontalCellsCount;
+    
+    [Tooltip("Vertical cells count must be even number")]
+    [SerializeField] private int _verticalCellsCount;
+    public int VerticalCellsCount => 
+      _verticalCellsCount % 2 == 0 
+        ? _verticalCellsCount 
+        : ++_verticalCellsCount;
 
-    [field: SerializeField] private float SessionTime { get; }
+    [field: SerializeField] public float SessionTime { get; private set; }
+
+#if UNITY_EDITOR
+    private void OnValidate() =>
+      ValidateVerticalCellsCount();
+
+    private void ValidateVerticalCellsCount()
+    {
+      if (_verticalCellsCount % 2 != 0)
+        _verticalCellsCount++;
+    }
+#endif
   }
 }
---
+++ src/MemoryCardsPrototype/Assets/Code/Runtime/CardGame/Data/ICardGameConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Code.Runtime.CardGame.Data
+{
+  public interface ICardGameConfiguration
+  {
+    public int HorizontalCellsCount { get; }
+    public int VerticalCellsCount { get; }
+    public float SessionTime { get; }
+  }
+}
