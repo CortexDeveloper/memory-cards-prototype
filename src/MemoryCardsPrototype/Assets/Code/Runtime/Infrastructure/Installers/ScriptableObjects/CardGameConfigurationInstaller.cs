23011269c942c793c5a9cb200526dd46cfd17652
unknown <cortexdeveloper@gmail.com>
2021-01-17 17:40:31 +0200

    add cared game config installer, setup on scene context

---
+++ src/MemoryCardsPrototype/Assets/Code/Runtime/Infrastructure/Installers/ScriptableObjects/CardGameConfigurationInstaller.cs
@@ -0,0 +1,19 @@
+﻿using Code.Runtime.CardGame.Data;
+using UnityEngine;
+using Zenject;
+
+namespace Code.Runtime.Infrastructure.Installers.ScriptableObjects
+{
+  [CreateAssetMenu(fileName = nameof(CardGameConfigurationInstaller), menuName = "CardGame/ScriptableObjectInstallers/" + nameof(CardGameConfigurationInstaller))]
+  public class CardGameConfigurationInstaller : ScriptableObjectInstaller
+  {
+    [SerializeField] private ScriptableObject Configuration;
+    
+    public override void InstallBindings()
+    {
+      Container
+        .Bind<ICardGameConfiguration>()
+        .FromInstance(Configuration as ICardGameConfiguration);
+    }
+  }
+}
